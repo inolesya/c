cb19c2eb7a7b36a85a4c2961c43ce1acbb6501c2
pgsin <pgsinitsyn@gmail.com>
2014-04-25 23:37:21 +0400

    Отлично написано - внес мелкие поправки

--- 3_3.c
+++ 3_3.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
-#define MAXLEN 50
+#define MAXLEN 100
 
 void expand(char s1[], char s2[]);
 
 int main()
 {
-	char in[MAXLEN] = "-1-a-a-f-27-g-9-\0";
+	//char in[MAXLEN] = "-1-a-a-f-27-g-9-\0";
+	//char in[MAXLEN] = "a-zA-Z0-9\0";
+	char in[MAXLEN] = "a-z0-9\0";
 	char out[MAXLEN];
 	printf("s1 = %s\n", in);
 	expand(in, out);
@@ -20,24 +22,24 @@ void expand(char s1[], char s2[])
 	int i, j;
 	char t;
 	for (i = 0, j = 0; s1[i] != '\0'; i++) {
-		if ( isdigit(s1[i]) && s1[i+1] == '-' &&
-									 isdigit(s1[i+2]) ) {
-			for (t = s1[i]; t < s1[i+2]; t++)
-					s2[j++] = t;
-					i += 1;    
-			}
-		else if ( islower(s1[i]) && s1[i+1] == '-' &&
-                                      islower(s1[i+2]) ) {
+		if ( isdigit(s1[i]) && s1[i+1] == '-' &&\
+                        isdigit(s1[i+2]) ) {
 			for (t = s1[i]; t < s1[i+2]; t++)
 				s2[j++] = t;
-				i++;    
-			}
-		else if ( isupper(s1[i]) && s1[i+1] == '-' &&
-                                      isupper(s1[i+2]) ) {
+            i++;
+		}
+		else if ( islower(s1[i]) && s1[i+1] == '-' &&\
+                        islower(s1[i+2]) ) {
 			for (t = s1[i]; t < s1[i+2]; t++)
 				s2[j++] = t;
-				i++;    
-			}
+            i++;
+		}
+		else if ( isupper(s1[i]) && s1[i+1] == '-' &&\
+                        isupper(s1[i+2]) ) {
+			for (t = s1[i]; t < s1[i+2]; t++)
+				s2[j++] = t;
+            i++;
+		}
 		else
 			s2[j++] = s1[i];
 	}
