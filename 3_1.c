af71f1858116653dd85b4acd3543e1f80e089a5d
pgsin <pgsinitsyn@gmail.com>
2014-04-25 15:55:56 +0400

    Есть куча замечаний по поводу оформления. Не стоит писать функцию внутри другой. Ты проверяла работоспособность программы? По стилю оформления кажется, что нет. Смотри изменения

--- 3_1.c
+++ 3_1.c
@@ -2,22 +2,27 @@
 
 main()
 {
-int provem(int x, int v[], int n) {
-      int low, high, mid;
+    int arr[8]={0,1,2,3,4,5,6,7};
+    int i;
+    printf("base\tposition\n");
+    for (i = 0; i<10; i++)
+        printf("%d\t%d\n",i,binSearch(i,arr,sizeof(arr)));
 
-      low = 0;
-      high = n - 1;
-      mid = (low+high) / 2;
-      while ( low <= high && x != v[mid] ) {
-          if ( x < v[mid] )
-                high = mid - 1;
-          else
-              low = mid + 1;
-          mid = (low+high) / 2;
-      }
-      if ( x == v[mid] )
-          return mid;
-      else
-          return -1; 
-  }
+}
+
+int binSearch(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while ( low <= high && x != v[(mid=(low+high)/2)] ) {
+        if ( x < v[mid] )
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    if ( x == v[mid] )
+        return mid;
+    else
+        return -1; 
 }
